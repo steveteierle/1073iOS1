bd9e3a1d77ab3284e160eebf73cd1998d1c09d7a
spaceLenny <spaceLenny@gmail.com>
2013-01-15 13:51:45 -0500

    Some changes

--- Thursday/THAppDelegate.h
+++ Thursday/THAppDelegate.h
@@ -2,18 +2,21 @@
 //  THAppDelegate.h
 //  Thursday
 //
-//  Created by David Evans on 1/10/13.
 //  Copyright (c) 2013 DavidEvans. All rights reserved.
 //
+
 
 #import <UIKit/UIKit.h>
 
 @class THViewController;
 
 @interface THAppDelegate : UIResponder <UIApplicationDelegate>
-
 @property (strong, nonatomic) UIWindow *window;
-
 @property (strong, nonatomic) THViewController *viewController;
 
 @end
+
+
+
+//Somehings different here
+
